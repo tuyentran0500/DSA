d2edcf6ce5b5c7ec9ce7d824e5ba2db34b721ac8
tuyentb <tuyentb@kikaitech.io>
2019-09-26 09:02:02 +0700

    Educational Round 73E

--- Edu73E.cpp
+++ Edu73E.cpp
@@ -3,6 +3,15 @@ using namespace std;
 int q;
 int a,b;
 string s;
+bool isDanger(int x){
+	if (x < b) return false;
+	if (x >= b && x < a) return true;
+	if (x >= 2*b) return true;
+	return false;
+}
+bool isSafe(int x){
+	return (x >= a && x < 2*b);
+}
 void solve(){
 	vector<int> list;
 	int curLen = 0;
@@ -16,18 +25,35 @@ void solve(){
 			}
 		}
 	}
+	if (curLen) list.push_back(curLen);
 	int cnt_ba = 0; // b <= x <= a
-	int cnt_2b = 0; // 2b <= x.
-	int cnt_a = 0; // a <= x
+	int cnt_a2b = 0; // 2b <= x.
+	int cnt_2b = 0;
+	int val = 0;
 	for (int i=0;i<list.size();++i){
+		if (list[i] < b) continue;
 		if (list[i] >= b && list[i] < a) cnt_ba++;
-		if (list[i] >= 2*b) cnt_2b++;
-		if (list[i] >= a) cnt_a++;
+		else if (list[i] < 2*b) cnt_a2b++;
+			else {
+				cnt_2b++; 
+				val = list[i];
+			}
 	}
 	if (cnt_ba) cout << "No\n";
 	else {
 		if (cnt_2b >= 2) cout << "No\n";
-		else cout << (cnt_a & 1 ? "Yes" : "No") << '\n';
+		else {
+			if (val){
+				bool ok = false;
+				for(int i=0;i+a <= val;++i){
+					if (isDanger(i) || isDanger(val-i-a)) continue;
+					if ((cnt_a2b + isSafe(i) + isSafe(val-i-a)) % 2 == 0) ok = true;
+				}
+				if (ok == true) cout << "Yes\n";
+				else cout << "No\n";
+			}
+			else cout << (cnt_a2b % 2 ? "Yes" : "No") << '\n';
+		}
 	}
 }
 int main(){
