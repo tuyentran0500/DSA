80bedc8e41cb36b2f042c372f7ae9bffdeabd7fd
Tuyen Tran <tuyentran0500@gmail.com>
2019-11-02 00:21:06 +0700

    update new version to generate Hackerrank Testcase.

--- sys_gen.cpp
+++ sys_gen.cpp
@@ -1,11 +1,38 @@
 #include <bits/stdc++.h>
-#define up(i,a,b) for(int (i) = (a);(i)<=(b);++(i))
-#define down(i,b,a) for(int (i) = (b);i>=(a);--i)
-#define debug(x) cerr << (x) << '\n';
-#define bits(x,i) ((x >> i) & 1)
 using namespace std;
-const int N = 10;
-int n;
+// Tên chương trình
+// Số test kiểm tra
+const int NTEST = 50;
+string convert(int n,int maxTest){
+	string ans = "";
+	while (n){
+        char c = '0' + (n%10);
+		ans = (char)(n % 10 + '0') + ans;
+		n/=10;
+	}
+	while (ans.size() < maxTest) ans = "0" + ans;
+	return ans;
+}
 int main(){
-	ios_base::sync_with_stdio(0); cin.tie(0);
+    srand(time(NULL));
+    system("rmdir /s Test");
+    system("mkdir Test");
+    system("mkdir input");
+    system("mkdir output");
+    for(int iTest = 1; iTest <= NTEST; iTest++)
+    {
+    	// run solve and gen.
+        system("gen.exe");
+        system("solve.exe");
+        // Nếu dùng linux thì thay fc bằng diff
+        string inp = "input" + convert(iTest,2) + ".txt";
+        system(("rename solve.inp " + inp).c_str());
+        system(("move " + inp + " input").c_str());
+        string out = "output" + convert(iTest,2) + ".txt";
+        system(("rename solve.OUT " + out).c_str());
+        system(("move " + out + " output").c_str());
+    }
+    system("move input Test");
+    system("move output Test");
+    return 0;
 }
